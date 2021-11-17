d1f82c5130945007c195a46a662123edb54a405d
KAVIYASS99 <94182282+KAVIYASS99@users.noreply.github.com>
2021-11-17 21:04:21 +0530

    Add files via upload

---
+++ Main_header.c
@@ -0,0 +1,35 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "includes.h"
+
+#define PPhNum				"YOUR_PH_NUMBER"
+#define FPhNum				"YOUR_PH_NUMBER"
+
+#define INTDDR				DDRD
+#define INTPORT				PORTD
+#define INT0_PIN			PD2
+
+#define SPRINK_DDR			DDRD
+#define SPRINK_PORT			PORTD
+#define SPRINK_PIN			PD6
+#define SHOCK_PIN			PD5
+ 
+#define TIMER2_RELOAD		5       
+#define SPRINK_TIME			1000
+
+#define StartTmr()			TCCR0  	|= _BV(CS01)
+#define StopTmr()			TCCR0  	&= ~_BV(CS01)
+
+#define SprinkleOn()		SPRINK_PORT |= _BV(SPRINK_PIN)
+#define SprinkleOff()		SPRINK_PORT &= ~_BV(SPRINK_PIN)
+
+#define ShockOn()			SPRINK_PORT |= _BV(SHOCK_PIN)
+#define ShockOff()			SPRINK_PORT &= ~_BV(SHOCK_PIN)
+
+static void	init		(void);
+static void	disptitl 	(void);
+static void	EXTINTinit	(void);
+static void	Tmr2Init	(void) ;
+
+#endif
